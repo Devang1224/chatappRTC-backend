644b5a13a7d598fa7f6e9e9e2345a665ee9cc08a
Devang1224 <97470914+Devang1224@users.noreply.github.com>
2023-07-14 21:32:33 +0530

    updated socket

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const http = require('http');
 const app = express();
 
 app.use(cors({
-    origin: "https://chatrtc.netlify.app"
+    origin: "http://localhost:3001"
   }));
 app.use(express.json());
 
@@ -20,7 +20,7 @@ connectDB(); //establishing mongodb connection
 const server = http.createServer(app);
 const io = socketio(server, {
     cors: {
-      origin: "https://chatrtc.netlify.app",
+      origin: "http://localhost:3001",
       methods: ["GET", "POST"]
     }
   });;
@@ -30,7 +30,8 @@ const io = socketio(server, {
 app.use("/user",userRoute);
 app.use("/chat",conversationRoute)
 
-
+const userToSocket = new Map();
+const socketToUser = new Map();
 
 io.on("connection", (socket) => {
 
@@ -38,19 +39,56 @@ io.on("connection", (socket) => {
 
     socket.emit("connected","message from the server")
    
-    socket.on("privateChat",(id)=>{
-        socket.join(id);
-        console.log((`user connected with convo id :${id}`));
+    socket.on("privateChat",(data)=>{  
+      const{convoid:roomId,username:User} = data;          // roomId is convoId  ( destructring of objects)
+        userToSocket.set(User,socket.id);
+        socketToUser.set(socket.id,User);
+        socket.join(roomId);
+        console.log((`user connected with convo id :${roomId}`));
+
+
     })
- 
+      
+   // when a user press the videocall button 
+    socket.on("enterVideoCall",(data)=>{
+      const{convoId:roomId,User} = data;          // roomId is convoId  ( destructring of objects)
+    socket.broadcast.to(roomId).emit("user-joined",{User,id:socket.id});
+
+    })
+
+socket.on("user:call", ({ to, offer }) => {
+  io.to(to).emit("incomming:call", { from: socket.id, offer });
+});
+
+socket.on("call:accepted", ({ to, ans }) => {
+  io.to(to).emit("call:accepted", { from: socket.id, ans });
+});
+
+socket.on("peer:nego:needed", ({ to, offer }) => {
+  console.log("peer:nego:needed", offer);
+  io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+});
+
+socket.on("peer:nego:done", ({ to, ans }) => {
+  console.log("peer:nego:done", ans);
+  io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+});
+
+
+
+
+
+
+
+
+    //sending messages
     socket.on("newMessage",(message)=>{
-
-        console.log(message.conversationId);
-
-        // socket.emit("Message",message);
-  io.to(message.conversationId).emit('Message', message);
+       io.to(message.conversationId).emit('Message', message);
     })
+    //sending messages
 
   });
 
-  server.listen(process.env.PORT || 6010, () => console.log(`Server has started.`));
+
+  server.listen(process.env.PORT , () => console.log(`Server has started.`));
+  // || 6010
